5c2132c2d698287de72c9c8e503073236dca32e3
liulin <1002315926@qq.com>
2019-05-12 20:18:09 +0800

    动态代理

---
+++ src/proxy/dynamic/PersonBean.java
@@ -0,0 +1,12 @@
+package proxy.dynamic;
+
+public interface PersonBean {
+    String getName();
+    String getGender();
+    String getInterests();
+    int getHotOrNotRating();
+    void setName(String name);
+    void setGender(String gender);
+    void setInterests(String interests);
+    void setHotOrNotRating(int hotOrNotRating);
+}
